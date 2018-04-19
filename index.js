9ea2cedeb65586f8473e0e967f1858b57c2e10db
Jonathan Lee <51lverfangster@gmail.com>
2018-04-18 19:07:51 -0700

    (added) index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+const { body } = document;
+const style = window.getComputedStyle(body);
+const bgSrc = style.backgroundImage;
+const bgUrl = bgSrc.match(/\((.*?)\)/)[1].replace(/('|")/g,'');
+const img = new Image();
+img.onload = () => {
+  body.setAttribute('class', 'fade-in-element');
+}
+img.src = bgUrl;
+if (img.complete) {
+  img.onload();
+};
+
+const projectLink = document.getElementById('project-scroll');
+const projectContainer = document.getElementById('project-container');
+
+projectLink.addEventListener('click', () => {
+  const options = {
+    behavior: 'smooth',
+    block: 'start',
+    inline: 'nearest',
+  };
+  if (projectContainer.className.includes('hidden')) {
+    projectContainer.setAttribute('class', 'fade-in-element');
+  };
+  projectContainer.scrollIntoView(options);
+});
